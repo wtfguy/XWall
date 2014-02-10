1ac2e4f221ea275084506a186591c956fe9eeca4
wtfguy <novalid@email.com>
2014-02-11 00:24:20 +0100

    Integer.class -> int.class

--- src/de/wtfguy/android/xwall/XWall.java
+++ src/de/wtfguy/android/xwall/XWall.java
@@ -1,7 +1,6 @@
 package de.wtfguy.android.xwall;
 
 import java.io.FileDescriptor;
-import java.lang.reflect.Method;
 import java.net.InetAddress;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -29,30 +28,9 @@ public class XWall implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 //		String className = "libcore.io.IoBridge";
 		String className = "libcore.io.ForwardingOs";
 		String methodName = "connect";
-		Class[] methodParams = new Class[]{ FileDescriptor.class, InetAddress.class, Integer.class };
 		
-		
-		/* revo89 suggested not to use findMethodBestMatch but this is currently the only way to not get MethodNotFoundException */
-		try {
-			Class<?> clazz = XposedHelpers.findClass( className, null);
-//			Class<?> clazz = XposedHelpers.findClass("libcore.io.IoBridge", null);
-			XposedBridge.log(  clazz.getName() + " found!" );
-			
-			Method method = XposedHelpers.findMethodBestMatch( clazz, methodName, methodParams );
-//			Method method = clazz.getDeclaredMethod( methodName, methodParams );
-//			method.setAccessible( true );
-			XposedBridge.log( method.getName() + " found!" );
-			
-			XposedBridge.hookMethod( method, getXcMethodHook() );
-			
-		} catch (Throwable ex) {
-			XposedBridge.log( ex );
-		}
-		
-		
-//		Class clazz = Class.forName( "libcore.io.ForwardingOs" );
-//		String methodName = "connect";
-//		XposedHelpers.findAndHookMethod( clazz, methodName, FileDescriptor.class, InetAddress.class, Integer.class, getXcMethodHook() );
+		Class<?> clazz = Class.forName( className );
+		XposedHelpers.findAndHookMethod( clazz, methodName, FileDescriptor.class, InetAddress.class, int.class, getXcMethodHook() );
 	}
 
 	
