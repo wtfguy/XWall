d984479ece7f41f4e3460a14049632cf3846137f
wtfguy <novalid@email.com>
2014-02-11 00:05:42 +0100

    tried a few things

--- src/de/wtfguy/android/xwall/XWall.java
+++ src/de/wtfguy/android/xwall/XWall.java
@@ -3,7 +3,6 @@ package de.wtfguy.android.xwall;
 import java.io.FileDescriptor;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
-import java.net.SocketException;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
@@ -26,27 +25,50 @@ public class XWall implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 		configurator.logRules();
 		
 		XposedBridge.log(  "HOOKing " + startupParam.modulePath );
-			
+		
+//		String className = "libcore.io.IoBridge";
+		String className = "libcore.io.ForwardingOs";
+		String methodName = "connect";
+		Class[] methodParams = new Class[]{ FileDescriptor.class, InetAddress.class, Integer.class };
+		
+		
+		/* revo89 suggested not to use findMethodBestMatch but this is currently the only way to not get MethodNotFoundException */
 		try {
-//			Class<?> clazz = XposedHelpers.findClass("libcore.io.ForwardingOs", null);
-			Class<?> clazz = XposedHelpers.findClass("libcore.io.IoBridge", null);
+			Class<?> clazz = XposedHelpers.findClass( className, null);
+//			Class<?> clazz = XposedHelpers.findClass("libcore.io.IoBridge", null);
 			XposedBridge.log(  clazz.getName() + " found!" );
-			Method method = XposedHelpers.findMethodBestMatch( clazz, "connect", FileDescriptor.class, InetAddress.class, Integer.class );
-			XposedBridge.log(  method.getName() + " found!" );
+			
+			Method method = XposedHelpers.findMethodBestMatch( clazz, methodName, methodParams );
+//			Method method = clazz.getDeclaredMethod( methodName, methodParams );
+//			method.setAccessible( true );
+			XposedBridge.log( method.getName() + " found!" );
+			
 			XposedBridge.hookMethod( method, getXcMethodHook() );
+			
 		} catch (Throwable ex) {
 			XposedBridge.log( ex );
 		}
-			
+		
+		
+//		Class clazz = Class.forName( "libcore.io.ForwardingOs" );
+//		String methodName = "connect";
+//		XposedHelpers.findAndHookMethod( clazz, methodName, FileDescriptor.class, InetAddress.class, Integer.class, getXcMethodHook() );
 	}
 
+	
+	@Override
+	public void handleLoadPackage( LoadPackageParam lpparam ) throws Throwable {
+		//currently unused
+	}
+
+	
 	
 	private XC_MethodHook getXcMethodHook() {
 		 return new XC_MethodHook() {
 			 @Override
 			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 				 XposedBridge.log(  "before connect..." );
-				 throw new SocketException( "Blocked by Firewall" );
+//				 throw new SocketException( "Blocked by Firewall" );
 			}
 			 
 			 @Override
@@ -55,12 +77,4 @@ public class XWall implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 			}
 		 };
 	}
-	
-	
-	
-	@Override
-	public void handleLoadPackage( LoadPackageParam lpparam ) throws Throwable {
-		
-	}
-	
 }
