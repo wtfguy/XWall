bda1bfded8b25bf4d4c7a2ec783e30689af4a3d0
wtfguy <novalid@email.com>
2014-02-14 16:36:45 +0100

    - new Icon - new service with toast support - hook for BlockGuardOs working

--- src/de/wtfguy/android/xwall/XWall.java
+++ src/de/wtfguy/android/xwall/XWall.java
@@ -1,36 +1,36 @@
 package de.wtfguy.android.xwall;
 
-import java.io.FileDescriptor;
-import java.net.InetAddress;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodHook.Unhook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
-import de.wtfguy.android.xwall.config.Configurator;
+import de.wtfguy.android.xwall.hook.AbstractHook;
+import de.wtfguy.android.xwall.hook.HookBlockGuardOsConnect;
+import de.wtfguy.android.xwall.util.Log;
 
 public class XWall implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 
-	private Configurator configurator;
+	public static final String LOG_TAG = "XWall";
+	
+	private Log log = Log.getLogger();
 	
 	@Override
 	public void initZygote( StartupParam startupParam ) throws Throwable {
-		XposedBridge.log( "Module: " + startupParam.modulePath );
-	
-		configurator = new Configurator();
-		configurator.loadRules();
-		configurator.logRules();
+		log.info( "Init hooking for module " + startupParam.modulePath );
 		
-		XposedBridge.log(  "HOOKing " + startupParam.modulePath );
+		registerServices();
 		
-//		String className = "libcore.io.IoBridge";
-		String className = "libcore.io.ForwardingOs";
-		String methodName = "connect";
-		
-		Class<?> clazz = Class.forName( className );
-		XposedHelpers.findAndHookMethod( clazz, methodName, FileDescriptor.class, InetAddress.class, int.class, getXcMethodHook() );
+		hook( new HookBlockGuardOsConnect() );
 	}
 
 	
@@ -40,19 +40,48 @@ public class XWall implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 	}
 
 	
-	
-	private XC_MethodHook getXcMethodHook() {
-		 return new XC_MethodHook() {
-			 @Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				 XposedBridge.log(  "before connect..." );
-//				 throw new SocketException( "Blocked by Firewall" );
-			}
-			 
-			 @Override
-			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				 XposedBridge.log(  "after connect..." );
-			}
-		 };
+	private void registerServices() {
+		// System server
+		try {
+			// frameworks/base/services/java/com/android/server/SystemServer.java
+			Class<?> cSystemServer = findClass("com.android.server.SystemServer", null);
+			Method mMain = cSystemServer.getDeclaredMethod("main", String[].class);
+			XposedBridge.hookMethod(mMain, new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					
+					XwallService.register();
+					
+				}
+			});
+		} catch (Throwable ex) {
+			log.error(null, ex);
+		}
 	}
+	
+	
+	private Unhook hook( final AbstractHook hook ) {
+		try {
+			List<Object> xposedParameters = new ArrayList<Object>();
+			xposedParameters.addAll( Arrays.asList( hook.getMethodParameters() ) );
+			xposedParameters.add( new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					log.debug( "delegating beforeHookedMethod!" );
+					hook.beforeHookedMethod(param);
+				}
+				
+				@Override
+				protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+					log.debug( "delegating afterHookedMethod!" );
+					hook.afterHookedMethod(param);
+				}
+			} );
+			return XposedHelpers.findAndHookMethod( Class.forName( hook.getClassName() ), hook.getMethodName(), xposedParameters.toArray() );
+		} catch( ClassNotFoundException e ) {
+			log.error( "Unable to register Hook", e );
+			return null;
+		}
+	}
+	
 }
